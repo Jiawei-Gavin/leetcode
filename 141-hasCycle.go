d2991118f1bae79ea0acb5a4f71c36569b5abddd
Jiawei Lu <ljw9712@163.com>
2021-05-06 18:13:56 +0100

    hasCycle

---
+++ 141-hasCycle.go
@@ -0,0 +1,19 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	fast := head
+	slow := head
+	for fast != nil && slow != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
