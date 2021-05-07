5ecbb31234465a93ca69896012222a60a402fac4
Jiawei Lu <ljw9712@163.com>
2021-05-07 09:08:36 +0100

    isSymmetric

---
+++ 101-isSymmetric.go
@@ -0,0 +1,24 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	return check(root, root)
+}
+
+func check(root1, root2 *TreeNode) bool {
+	if root1 == nil && root2 == nil {
+		return true
+	}
+	if root1 == nil || root2 == nil {
+		return false
+	}
+	if root1.Val != root2.Val {
+		return false
+	}
+	return check(root1.Left, root2.Right) && check(root1.Right, root2.Left)
+}
