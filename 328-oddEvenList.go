cb6d040ca97e77f005dde22040dea3d0ee8aedb3
Jiawei Lu <ljw9712@163.com>
2021-05-09 13:31:16 +0100

    oddEvenList

---
+++ 328-oddEvenList.go
@@ -0,0 +1,23 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	odd := head
+	even := head.Next
+	evenHead := even
+	for even != nil && even.Next != nil {
+		odd.Next = even.Next
+		odd = odd.Next
+		even.Next = odd.Next
+		even = even.Next
+	}
+	odd.Next = evenHead
+	return head
+}
