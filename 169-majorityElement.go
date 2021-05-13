d5ffdba7a421f75242e7e0eb1c11e27100b60dc7
Jiawei Lu <ljw9712@163.com>
2021-05-13 15:20:49 +0100

    majorityElement

---
+++ 169-majorityElement.go
@@ -0,0 +1,30 @@
+package leetcode
+
+// solution1
+func majorityElement(nums []int) int {
+	hashTable := make(map[int]int)
+	for _, val := range nums {
+		hashTable[val]++
+		if hashTable[val] > len(nums)/2 {
+			return val
+		}
+	}
+	return 0
+}
+
+// solution2
+func majorityElement(nums []int) int {
+	result, count := nums[0], 0
+	for i := 0; i < len(nums); i++ {
+		if count == 0 {
+			result, count = nums[i], 1
+		} else {
+			if nums[i] == result {
+				count++
+			} else {
+				count--
+			}
+		}
+	}
+	return result
+}
