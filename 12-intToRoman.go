9c7bba9b681b69b809e97c9743ca865ff7a27349
Jiawei Lu <ljw9712@163.com>
2021-05-13 22:44:51 +0100

    intToRoman

---
+++ 12-intToRoman.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func intToRoman(num int) string {
+	values := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	symbols := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+	var roman string
+	i := 0
+	for num != 0 {
+		for values[i] > num {
+			i++
+		}
+		num -= values[i]
+		roman += symbols[i]
+	}
+	return roman
+}
