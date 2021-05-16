5b65f0b703773ef37e2508041dcd2ed2f1311a4f
Jiawei Lu <ljw9712@163.com>
2021-05-16 21:44:29 +0100

    findDuplicates

---
+++ 442-findDuplicates.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func findDuplicates(nums []int) []int {
+	var result []int
+	for i := 0; i < len(nums); i++ {
+		if nums[abs(nums[i])-1] < 0 {
+			result = append(result, abs(nums[i]))
+		}
+		nums[abs(nums[i])-1] = -nums[abs(nums[i])-1]
+	}
+	return result
+}
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
