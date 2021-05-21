2d746b804f2e73f91ae9e33f12b73316626c7c63
Jiawei Lu <ljw9712@163.com>
2021-05-21 12:16:43 +0100

    mergeKLists

---
+++ 23-mergeKLists.go
@@ -0,0 +1,34 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	} else if len(lists) == 1 {
+		return lists[0]
+	}
+
+	mid := len(lists) / 2
+	left, right := mergeKLists(lists[:mid]), mergeKLists(lists[mid:])
+	return mergeTwoLists(left, right)
+
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	}
+	l2.Next = mergeTwoLists(l1, l2.Next)
+	return l2
+}
