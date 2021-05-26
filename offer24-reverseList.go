d77c2cc7c73d3e62eac7bd1cd9fbe4ce3704ffb4
Jiawei Lu <ljw9712@163.com>
2021-05-26 21:30:55 +0100

    reverseList

---
+++ offer24-reverseList.go
@@ -0,0 +1,17 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var cur *ListNode
+	for head != nil {
+		next := head.Next
+		head.Next = cur
+		cur = head
+		head = next
+	}
+	return cur
+}
