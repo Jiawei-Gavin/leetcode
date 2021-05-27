84e202b852d99680f899284305b23463e112cca8
Jiawei Lu <ljw9712@163.com>
2021-05-27 17:29:26 +0100

    maxValue

---
+++ offer47-maxValue.go
@@ -0,0 +1,26 @@
+package leetcode
+
+func maxValue(grid [][]int) int {
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if i == 0 && j == 0 {
+				continue
+			}
+			if i == 0 {
+				grid[i][j] += grid[i][j-1]
+			} else if j == 0 {
+				grid[i][j] += grid[i-1][j]
+			} else {
+				grid[i][j] += max(grid[i][j-1], grid[i-1][j])
+			}
+		}
+	}
+	return grid[len(grid)-1][len(grid[0])-1]
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
