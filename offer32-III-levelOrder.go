f3d736358cf4ce46afb2597c3e5a0a7726fada0a
Jiawei Lu <ljw9712@163.com>
2021-05-28 23:16:45 +0100

    levelOrder

---
+++ offer32-III-levelOrder.go
@@ -0,0 +1,65 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+var result [][]int
+
+// solution1
+func levelOrder(root *TreeNode) [][]int {
+	result = make([][]int, 0)
+	dfs(root, 0)
+	return result
+}
+
+func dfs(root *TreeNode, level int) {
+	if root == nil {
+		return
+	}
+	if len(result) == level {
+		result = append(result, []int{root.Val})
+	} else {
+		if level%2 == 0 {
+			result[level] = append(result[level], root.Val)
+		} else {
+			result[level] = append([]int{root.Val}, result[level]...)
+		}
+	}
+
+	dfs(root.Left, level+1)
+	dfs(root.Right, level+1)
+}
+
+// solution2
+func levelOrder(root *TreeNode) [][]int {
+	var result [][]int
+	if root == nil {
+		return result
+	}
+	var level int
+	var queue = []*TreeNode{root}
+	for 0 < len(queue) {
+		length := len(queue)
+		result = append(result, []int{})
+		for 0 < length {
+			length--
+			if queue[0].Left != nil {
+				queue = append(queue, queue[0].Left)
+			}
+			if queue[0].Right != nil {
+				queue = append(queue, queue[0].Right)
+			}
+			if level%2 == 0 {
+				result[level] = append(result[level], queue[0].Val)
+			} else {
+				result[level] = append([]int{queue[0].Val}, result[level]...)
+			}
+			queue = queue[1:]
+		}
+		level++
+	}
+	return result
+}
