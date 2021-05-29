6ef55e71619a68a58947a4b4827cce6834f25c9f
Jiawei Lu <ljw9712@163.com>
2021-05-29 21:43:31 +0100

    lowestCommonAncestor

---
+++ offer68-II-lowestCommonAncestor.go
@@ -0,0 +1,22 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root == q || root == p {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	}
+	if left == nil {
+		return right
+	}
+	return left
+}
