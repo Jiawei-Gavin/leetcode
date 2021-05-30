4981dd552b2665adaf91c95e83fc2675b6b1da47
Jiawei Lu <ljw9712@163.com>
2021-05-30 15:05:25 +0100

    isStraight

---
+++ offer61-isStraight.go
@@ -0,0 +1,18 @@
+package leetcode
+
+import "sort"
+
+func isStraight(nums []int) bool {
+	sort.Ints(nums)
+	sub := 0
+	for i := 0; i < 4; i++ {
+		if nums[i] == 0 {
+			continue
+		}
+		if nums[i] == nums[i+1] {
+			return false
+		}
+		sub += nums[i+1] - nums[i]
+	}
+	return sub < 5
+}
