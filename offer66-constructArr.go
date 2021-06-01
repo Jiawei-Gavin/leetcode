9a51dc1d3ee6b833623059a5d57471b56dcaf2ca
Jiawei Lu <ljw9712@163.com>
2021-06-01 15:54:26 +0100

    constructArr

---
+++ offer66-constructArr.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func constructArr(a []int) []int {
+	if len(a) == 0 {
+		return []int{}
+	}
+	b := make([]int, len(a))
+	b[0] = 1
+	tmp := 1
+	for i := 1; i < len(a); i++ {
+		b[i] = b[i-1] * a[i-1]
+	}
+	for i := len(a) - 2; i >= 0; i-- {
+		tmp *= a[i+1]
+		b[i] *= tmp
+	}
+	return b
+}
