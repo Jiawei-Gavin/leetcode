6f394cdc88ac2882dac7971b21e2c733e682cc9f
Jiawei Lu <ljw9712@163.com>
2021-06-06 16:35:55 +0100

    detectCycle

---
+++ 142-detectCycle.go
@@ -0,0 +1,25 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil {
+		if fast.Next == nil {
+			return nil
+		}
+		slow, fast = slow.Next, fast.Next.Next
+		if fast == slow {
+			p := head
+			for p != slow {
+				p = p.Next
+				slow = slow.Next
+			}
+			return p
+		}
+	}
+	return nil
+}
