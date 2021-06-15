c5a59fcc4190f6a3e8a83e5bf52d4c536bb5a2ae
Jiawei Lu <ljw9712@163.com>
2021-06-15 23:47:17 +0100

    searchMatrix

---
+++ 10-09-searchMatrix.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func searchMatrix(matrix [][]int, target int) bool {
+	for _, row := range matrix {
+		left, right := 0, len(matrix[0])-1
+		for left <= right {
+			mid := (left + right) >> 1
+			if row[mid] > target {
+				right = mid - 1
+			} else if row[mid] < target {
+				left = mid + 1
+			} else {
+				return true
+			}
+		}
+	}
+	return false
+}
