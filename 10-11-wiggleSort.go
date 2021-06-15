50b1574c4be97f1305d646e6cd52620dd0fb60e5
Jiawei Lu <ljw9712@163.com>
2021-06-15 23:52:55 +0100

    wiggleSort

---
+++ 10-11-wiggleSort.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func wiggleSort(nums []int) {
+	for i := 0; i < len(nums)-1; i++ {
+		if i%2 == 1 {
+			if nums[i] < nums[i+1] {
+				nums[i], nums[i+1] = nums[i+1], nums[i]
+			}
+		} else {
+			if nums[i] > nums[i+1] {
+				nums[i], nums[i+1] = nums[i+1], nums[i]
+			}
+		}
+	}
+}
