93b25c1d03220f66f57a4f2d98e6d4af1b59d4f6
Jiawei Lu <ljw9712@163.com>
2021-06-21 00:20:12 +0100

    findNumberOfLIS

--- 0673-findNumberOfLIS.go
+++ 0673-findNumberOfLIS.go
@@ -3,11 +3,13 @@ package leetcode
 func findNumberOfLIS(nums []int) int {
 	dp := make([]int, len(nums))
 	count := make([]int, len(nums))
-	var max int
-	for i := 0; i <= len(nums)-1; i += 1 {
+	dp[0] = 1
+	count[0] = 1
+	maxLength := 1
+	for i := 1; i < len(nums); i++ {
 		dp[i] = 1
 		count[i] = 1
-		for j := 0; j < i; j += 1 {
+		for j := 0; j < i; j++ {
 			if nums[i] > nums[j] {
 				if dp[j]+1 > dp[i] {
 					dp[i] = dp[j] + 1
@@ -17,16 +19,20 @@ func findNumberOfLIS(nums []int) int {
 				}
 			}
 		}
-		if max < dp[i] {
-			max = dp[i]
-		}
+		maxLength = max(maxLength, dp[i])
 	}
-
 	var res int
 	for i := 0; i <= len(dp)-1; i += 1 {
-		if dp[i] == max {
+		if dp[i] == maxLength {
 			res += count[i]
 		}
 	}
 	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
