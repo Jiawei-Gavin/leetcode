738860b56d19dfe447c4a3490a300c5b9a046a30
Jiawei Lu <ljw9712@163.com>
2021-06-21 13:45:04 +0100

    rob

--- 0213-rob.go
+++ 0213-rob.go
@@ -7,12 +7,17 @@ func rob(nums []int) int {
 	return max(rob1(nums[1:]), rob1(nums[:len(nums)-1]))
 }
 
+// 198
 func rob1(nums []int) int {
-	use, notUse := 0, 0
-	for _, v := range nums {
-		use, notUse = notUse+v, max(use, notUse)
+	if len(nums) == 1 {
+		return nums[0]
 	}
-	return max(use, notUse)
+	dp := make([]int, len(nums))
+	dp[0], dp[1] = nums[0], max(nums[1], nums[0])
+	for i := 2; i < len(nums); i++ {
+		dp[i] = max(dp[i-1], nums[i]+dp[i-2])
+	}
+	return dp[len(nums)-1]
 }
 
 func max(a int, b int) int {
