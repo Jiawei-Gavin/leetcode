2af06d38df9b4bc7928a78c60cc7b0ef2da28adc
Jiawei Lu <ljw9712@163.com>
2021-06-25 21:53:31 +0100

    postorderTraversal

---
+++ 0145-postorderTraversal.go
@@ -0,0 +1,47 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// solution1
+func postorderTraversal(root *TreeNode) []int {
+	var res []int
+	postorder(root, &res)
+	return res
+}
+
+func postorder(root *TreeNode, res *[]int) {
+	if root == nil {
+		return
+	}
+	postorder(root.Left, res)
+	postorder(root.Right, res)
+	*res = append(*res, root.Val)
+}
+
+// solution2
+func postorderTraversal(root *TreeNode) []int {
+	var res []int
+	var stack []*TreeNode
+	var prev *TreeNode
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if root.Right == nil || root.Right == prev {
+			res = append(res, root.Val)
+			prev = root
+			root = nil
+		} else {
+			stack = append(stack, root)
+			root = root.Right
+		}
+	}
+	return res
+}
