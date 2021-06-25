ecdb0e561495311e3208eb213aff5c760259b7e8
Jiawei Lu <ljw9712@163.com>
2021-06-25 20:57:03 +0100

    inorderTraversal

--- 0094-inorderTraversal.go
+++ 0094-inorderTraversal.go
@@ -6,6 +6,7 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// solution1
 func inorderTraversal(root *TreeNode) []int {
 	var result []int
 	inorder(root, &result)
@@ -18,4 +19,21 @@ func inorder(root *TreeNode, result *[]int) {
 		*result = append(*result, root.Val)
 		inorder(root.Right, result)
 	}
+}
+
+// solution2
+func inorderTraversal(root *TreeNode) []int {
+	var res []int
+	var stack []*TreeNode
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, root.Val)
+		root = root.Right
+	}
+	return res
 }
