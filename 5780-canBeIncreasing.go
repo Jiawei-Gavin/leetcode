00bc2df35f7f63bb49e122692fa9bd05c58cbf7d
Jiawei Lu <ljw9712@163.com>
2021-06-26 17:16:25 +0100

    canBeIncreasing

---
+++ 5780-canBeIncreasing.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func canBeIncreasing(nums []int) bool {
+	count := 0
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] >= nums[i+1] {
+			count++
+			if i > 0 && i < len(nums)-2 {
+				if nums[i+1] <= nums[i-1] && nums[i] >= nums[i+2] {
+					return false
+				}
+			}
+		}
+		if count > 1 {
+			return false
+		}
+	}
+	return true
+}
