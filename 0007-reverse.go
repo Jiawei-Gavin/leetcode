936c7396ce925309eeb1997b362bc204276fc07a
Jiawei Lu <ljw9712@163.com>
2021-06-28 17:23:10 +0100

    reverse

--- 0007-reverse.go
+++ 0007-reverse.go
@@ -1,13 +1,15 @@
 package leetcode
 
+import "math"
+
 func reverse(x int) int {
-	tmp := 0
+	res := 0
 	for x != 0 {
-		tmp = tmp*10 + x%10
+		if res < math.MinInt32/10 || res > math.MaxInt32/10 {
+			return 0
+		}
+		res = res*10 + x%10
 		x = x / 10
 	}
-	if tmp < -(1<<31) || tmp > 1<<31-1 {
-		return 0
-	}
-	return tmp
+	return res
 }
