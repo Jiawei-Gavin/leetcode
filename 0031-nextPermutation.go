760b79b3bd5c4460d7bdb673cc55e48dff4d9389
Jiawei Lu <ljw9712@163.com>
2021-06-29 17:48:44 +0100

    nextPermutation

---
+++ 0031-nextPermutation.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func nextPermutation(nums []int) {
+	i := len(nums) - 2
+	for i >= 0 && nums[i] >= nums[i+1] {
+		i--
+	}
+	if i >= 0 {
+		j := len(nums) - 1
+		for j >= 0 && nums[i] >= nums[j] {
+			j--
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+	reverse(nums[i+1:])
+}
+
+func reverse(nums []int) {
+	start, end := 0, len(nums)-1
+	for start < end {
+		nums[start], nums[end] = nums[end], nums[start]
+		start++
+		end--
+	}
+}
