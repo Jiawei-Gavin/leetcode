e22457bd73b96cd6cac09b818b7e82ea2fa1d074
Jiawei Lu <ljw9712@163.com>
2021-07-04 21:38:21 +0100

    levelOrderBottom

---
+++ 0107-levelOrderBottom.go
@@ -0,0 +1,29 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrderBottom(root *TreeNode) [][]int {
+	var res [][]int
+	var dfs func(node *TreeNode, level int)
+	dfs = func(node *TreeNode, level int) {
+		if node == nil {
+			return
+		}
+		if len(res) == level {
+			res = append(res, []int{node.Val})
+		} else {
+			res[level] = append(res[level], node.Val)
+		}
+		dfs(node.Left, level+1)
+		dfs(node.Right, level+1)
+	}
+	dfs(root, 0)
+	for i := 0; i < len(res)/2; i++ {
+		res[i], res[len(res)-1-i] = res[len(res)-1-i], res[i]
+	}
+	return res
+}
