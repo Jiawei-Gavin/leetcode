a7955b584d9eaf282bd085040884ae3c857e5891
Jiawei Lu <ljw9712@163.com>
2021-07-20 17:27:10 +0100

    twoSum

--- 0001-twoSum.go
+++ 0001-twoSum.go
@@ -3,9 +3,9 @@ package leetcode
 func twoSum(nums []int, target int) []int {
 	hashMap := make(map[int]int)
 	for i := 0; i < len(nums); i++ {
-		another := target - nums[i]
-		if _, ok := hashMap[another]; ok {
-			return []int{hashMap[another], i}
+		temp := target - nums[i]
+		if _, ok := hashMap[temp]; ok {
+			return []int{hashMap[temp], i}
 		}
 		hashMap[nums[i]] = i
 	}
