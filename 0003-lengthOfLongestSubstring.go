cd454c03197e7f621bf2ea362583a1adabec0d8c
Jiawei Lu <ljw9712@163.com>
2021-07-21 00:54:50 +0100

    lengthOfLongestSubstring

--- 0003-lengthOfLongestSubstring.go
+++ 0003-lengthOfLongestSubstring.go
@@ -1,44 +1,16 @@
 package leetcode
 
-// solution1
-func lengthOfLongestSubstring(s string) int {
-	if len(s) == 0 {
-		return 0
-	}
-	bitset := make([]bool, 256)
-	result, right, left := 0, 0, 0
-	for left < len(s) {
-		if bitset[s[right]] {
-			bitset[s[left]] = false
-			left++
-		} else {
-			bitset[s[right]] = true
-			right++
-		}
-		if result < right-left {
-			result = right - left
-		}
-
-		if left+result >= len(s) || right >= len(s) {
-			break
-		}
-	}
-	return result
-}
-
-// solution2
 func lengthOfLongestSubstring(s string) int {
 	hashMap := make(map[byte]int)
-	result := 0
+	res := 0
 	for start, end := 0, 0; end < len(s); end++ {
-		if p, ok := hashMap[s[end]]; ok {
-			start = max(p, start)
+		if i, ok := hashMap[s[end]]; ok {
+			start = max(i, start)
 		}
 		hashMap[s[end]] = end + 1
-		result = max(result, end-start+1)
-
+		res = max(res, end-start+1)
 	}
-	return result
+	return res
 }
 
 func max(a int, b int) int {
