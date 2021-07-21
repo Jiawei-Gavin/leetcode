84c63040b2ec130b0c5a60507848f10efc1bdc75
Jiawei Lu <ljw9712@163.com>
2021-07-21 11:21:02 +0100

    threeSum

--- 0015-threeSum.go
+++ 0015-threeSum.go
@@ -3,32 +3,31 @@ package leetcode
 import "sort"
 
 func threeSum(nums []int) [][]int {
-	hashMap := make(map[int]int)
-	for _, value := range nums {
-		hashMap[value]++
-	}
-	var sortNums []int
-	for key := range hashMap {
-		sortNums = append(sortNums, key)
-	}
-	sort.Ints(sortNums)
-	var result [][]int
-	for i := 0; i < len(sortNums); i++ {
-		if sortNums[i] == 0 && hashMap[sortNums[i]] >= 3 {
-			result = append(result, []int{sortNums[i], sortNums[i], sortNums[i]})
+	sort.Ints(nums)
+	var res [][]int
+	for i := 0; i < len(nums); i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
 		}
-		for j := i + 1; j < len(sortNums); j++ {
-			if sortNums[i]*2+sortNums[j] == 0 && hashMap[sortNums[i]] >= 2 {
-				result = append(result, []int{sortNums[i], sortNums[i], sortNums[j]})
-			}
-			if sortNums[i]+sortNums[j]*2 == 0 && hashMap[sortNums[j]] >= 2 {
-				result = append(result, []int{sortNums[i], sortNums[j], sortNums[j]})
-			}
-			remainder := 0 - sortNums[i] - sortNums[j]
-			if sortNums[j] < remainder && hashMap[remainder] > 0 {
-				result = append(result, []int{sortNums[i], sortNums[j], remainder})
+		left, right := i+1, len(nums)-1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if sum > 0 {
+				right--
+			} else if sum == 0 {
+				res = append(res, []int{nums[i], nums[left], nums[right]})
+				for left < right && nums[right] == nums[right-1] {
+					right--
+				}
+				for left < right && nums[left] == nums[left+1] {
+					left++
+				}
+				right--
+				left++
+			} else {
+				left++
 			}
 		}
 	}
-	return result
+	return res
 }
