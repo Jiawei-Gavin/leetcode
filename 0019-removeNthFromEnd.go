3aea58a6368c83b3bc9442da29329140d10613b9
Jiawei Lu <ljw9712@163.com>
2021-07-21 12:16:42 +0100

    removeNthFromEnd

--- 0019-removeNthFromEnd.go
+++ 0019-removeNthFromEnd.go
@@ -5,22 +5,6 @@ type ListNode struct {
 	Next *ListNode
 }
 
-// solution1
-func removeNthFromEnd(head *ListNode, n int) *ListNode {
-	dummy := &ListNode{0, head}
-	length := 0
-	for ; head != nil; head = head.Next {
-		length++
-	}
-	cur := dummy
-	for i := 0; i < length-n; i++ {
-		cur = cur.Next
-	}
-	cur.Next = cur.Next.Next
-	return dummy.Next
-}
-
-// solution2
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	dummy := &ListNode{0, head}
 	fast, slow := head, dummy
