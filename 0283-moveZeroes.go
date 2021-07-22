78344c4ae0db0f5ac743473d5c5238dccba369a2
Jiawei Lu <ljw9712@163.com>
2021-07-22 16:57:56 +0100

    moveZeroes

--- 0283-moveZeroes.go
+++ 0283-moveZeroes.go
@@ -1,25 +1,7 @@
 package leetcode
 
-// solution1
 func moveZeroes(nums []int) {
-	j, zero := 0, 0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == 0 {
-			zero++
-		} else {
-			nums[j] = nums[i]
-			j++
-		}
-	}
-	for i := len(nums) - zero; i < len(nums); i++ {
-		nums[i] = 0
-	}
-}
-
-// solution2
-func moveZeroes(nums []int) {
-	right := 0
-	for left := 0; left < len(nums); left++ {
+	for left, right := 0, 0; left < len(nums); left++ {
 		if nums[left] != 0 {
 			if left != right {
 				nums[left], nums[right] = nums[right], nums[left]
