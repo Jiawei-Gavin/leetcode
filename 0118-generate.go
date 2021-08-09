8fd67a62afc24e40a6df1f87b9b73aed0950823b
Jiawei Lu <ljw9712@163.com>
2021-08-09 11:52:11 +0100

    generate

--- 0118-generate.go
+++ 0118-generate.go
@@ -1,17 +1,21 @@
 package leetcode
 
 func generate(numRows int) [][]int {
-	var result [][]int
+	dp := make([][]int, numRows)
+	if numRows == 0 {
+		return dp
+	}
+	for i := range dp {
+		dp[i] = make([]int, i+1)
+	}
 	for i := 0; i < numRows; i++ {
-		var row []int
 		for j := 0; j < i+1; j++ {
 			if j == 0 || j == i {
-				row = append(row, 1)
-			} else if i > 1 {
-				row = append(row, result[i-1][j-1]+result[i-1][j])
+				dp[i][j] = 1
+			} else {
+				dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
 			}
 		}
-		result = append(result, row)
 	}
-	return result
+	return dp
 }
