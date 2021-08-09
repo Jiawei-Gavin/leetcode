da9759bce275174353d0139fc31dcfcb322d341c
Jiawei Lu <ljw9712@163.com>
2021-08-09 12:24:59 +0100

    canPartition

---
+++ 0416-canPartition.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func canPartition(nums []int) bool {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	if sum%2 == 1 {
+		return false
+	}
+	sum /= 2
+	dp := make([]bool, sum+1)
+	dp[0] = true
+	for i := 0; i < len(nums); i++ {
+		for j := sum; j >= 0; j-- {
+			if j-nums[i] >= 0 {
+				dp[j] = dp[j] || dp[j-nums[i]]
+			}
+		}
+	}
+	return dp[sum]
+}
