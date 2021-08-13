4a00d502aa9c577b4ea055416f0482cfc6e46889
Jiawei Lu <ljw9712@163.com>
2021-08-13 21:07:56 +0100

    findSecondMinimumValue

---
+++ 0671-findSecondMinimumValue.go
@@ -0,0 +1,25 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func findSecondMinimumValue(root *TreeNode) int {
+	res := -1
+	rootVal := root.Val
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil || res != -1 && node.Val >= res {
+			return
+		}
+		if node.Val > rootVal {
+			res = node.Val
+		}
+		dfs(node.Left)
+		dfs(node.Right)
+	}
+	dfs(root)
+	return res
+}
